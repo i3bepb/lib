8b2ff873d9b94bcbbd6119eeeafd5c8e6776e247
i3bepb <g3bepb@gmail.com>
2017-01-12 16:29:33 +0500

    В debug() если переменная равна null, то выводить NULL

--- develop.php
+++ develop.php
@@ -1,27 +1,31 @@
 <?php
 namespace i3bepb;
-class develop {
-    static public function log($text) {
+class develop
+{
+    static public function log($text)
+    {
         $f = Func::rootDir() . '/mylog.txt';
-        if(!$f) exit('not exist mylog.txt');
+        if (!$f) exit('not exist mylog.txt');
         $fp = fopen($f, 'a') or die('err my log');
         fputs($fp, $text . "\r\n");
         fclose($fp);
     }
 
-    static public function debug($var, $output = true) {
+    static public function debug($var, $output = true)
+    {
         $o = '';
-        if($output) $o .= '<pre>';
-        if(is_bool($var)) {
+        if ($output) $o .= '<pre>';
+        if (is_bool($var)) {
             $o .= $var ? 'TRUE' : 'FALSE';
-        } elseif(!$var && is_scalar($var)) {
+        } elseif (is_null($var)) {
+            $o .= 'NULL';
+        } elseif (!$var && is_scalar($var)) {
             $o .= var_export($var, true);
         } else {
             $o .= print_r($var, true);
         }
-        if($output) $o .= '</pre>';
-
-        if($output) {
+        if ($output) $o .= '</pre>';
+        if ($output) {
             exit($o);
         } else {
             self::log($o);
